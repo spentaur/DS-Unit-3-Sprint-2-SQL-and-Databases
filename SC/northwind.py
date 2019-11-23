c57784b7927d574c5117c08b8990daa6aaa4c1f2
Spencer Adams <spencer.adams93@gmail.com>
2019-11-22 22:00:11 -0600

    part 2

--- SC/northwind.py
+++ SC/northwind.py
@@ -1,3 +1,25 @@
 from SQLite import SQLite
 
 
+def main():
+    db = "northwind_small.sqlite3"
+    with SQLite(db) as c:
+        ten_most_expensive_products = "SELECT ProductName, UnitPrice " \
+                                      "FROM Product " \
+                                      "ORDER BY UnitPrice " \
+                                      "DESC " \
+                                      "LIMIT 10"
+
+        c.execute(ten_most_expensive_products)
+        print(c.fetchall())
+        print("\n")
+
+        average_employee_age_at_hire_time = "SELECT AVG(HireDate - " \
+                                            "BirthDate) FROM Employee"
+        c.execute(average_employee_age_at_hire_time)
+        print(c.fetchone()[0])
+        print("\n")
+
+
+if __name__ == '__main__':
+    main()
